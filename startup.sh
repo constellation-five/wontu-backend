80623f95b0b78062350ffaa4d5eb2b1028a0ccfc
joshmoritty <josh.moritty@gmail.com>
2026-07-13 08:48:50 +0700

    chore: add scripts and confs to run reverb in the same app service

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
+
+nohup php /home/site/wwwroot/artisan queue:work > /home/site/wwwroot/storage/logs/queue.log 2>&1 &
+
+nohup php /home/site/wwwroot/artisan reverb:start --host=127.0.0.1 --port=8123 > /home/site/wwwroot/storage/logs/reverb.log 2>&1 &
