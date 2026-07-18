c0974462c15b75b7f2b81674c55a3be31c71af6e
joshmoritty <josh.moritty@gmail.com>
2026-07-18 17:49:25 +0700

    fix: reload nginx service after copying

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp /home/site/wwwroot/default /etc/nginx/sites-available/default
 
+service nginx reload
+
 nohup php /home/site/wwwroot/artisan queue:work > /home/site/wwwroot/storage/logs/queue.log 2>&1 &
 
 nohup php /home/site/wwwroot/artisan reverb:start --host=127.0.0.1 --port=8123 > /home/site/wwwroot/storage/logs/reverb.log 2>&1 &
