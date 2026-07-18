61f92d95333079035109784635985e4ead5f7e4e
joshmoritty <josh.moritty@gmail.com>
2026-07-18 18:00:35 +0700

    fix: make logs folder

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@ cp /home/site/wwwroot/default /etc/nginx/sites-available/default
 
 service nginx reload
 
+mkdir -p /home/site/wwwroot/storage/logs
+
 nohup php /home/site/wwwroot/artisan queue:work > /home/site/wwwroot/storage/logs/queue.log 2>&1 &
 
 nohup php /home/site/wwwroot/artisan reverb:start --host=127.0.0.1 --port=8123 > /home/site/wwwroot/storage/logs/reverb.log 2>&1 &
