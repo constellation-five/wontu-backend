dff4f7ace767aac5352e23328feb9ba6a9cbf460
Josh Rolando Moritty <josh.moritty@gmail.com>
2026-07-22 08:08:52 +0700

    Revert "fix: google callback"

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -40,7 +40,7 @@ class GoogleAuthController extends Controller
             session()->regenerate();
             session()->save();
 
-            return redirect(config('app.frontend_callback_url'));
+            return redirect(env('FRONTEND_CALLBACK_URL', ''));
         }
 
         // User doesn't exist. Store Google info in session and prompt for username to complete registration.
@@ -53,7 +53,7 @@ class GoogleAuthController extends Controller
             ],
         ]);
 
-        return redirect(config('app.frontend_register_url'));
+        return redirect(env('FRONTEND_REGISTER_URL', ''));
     }
 
     public function getPendingUser()
